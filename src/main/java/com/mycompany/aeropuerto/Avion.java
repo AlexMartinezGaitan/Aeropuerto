25174836b03b989a6c8ca6ac840269709b3800c7
EDGAR ALEXANDER MARTINEZ GAITAN <ealexandermartinez@ucundinamarca.edu.co>
2019-03-20 08:56:09 -0500

    se agrega la clase avion

---
+++ src/main/java/com/mycompany/aeropuerto/Avion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aeropuerto;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alexander Martinez
+ */
+public class Avion {
+    private int idAvion;
+    private int nSillas;
+    private String Marca;
+    private ArrayList<Silla> Silla;
+}
